d4e84f23a0b212d0369494edc907f9d770a1f242
Amar Tumballi <amar@dhiway.com>
2022-08-18 09:05:56 +0530

    update the example curl command script
    Signed-off-by: Amar Tumballi <amar@dhiway.com>

--- extras/curl-commands.sh
+++ extras/curl-commands.sh
@@ -1,11 +1,7 @@
-curl --request POST http://localhost:5001/api/v1/spaces --header 'Content-Type: application/json' --data-raw '{ "space": { "title": "test", "description": "ABCD" }}'
-Output :
-{"result":"SUCCESS","space":"space:cord:4B9Ahyjif9L8UGtoUcGdaS6TbCeMhq9XHAZ6rkmQdPmMtV8K"}
 
 curl --request POST http://localhost:5001/api/v1/schemas \
      --header 'Content-Type: application/json' \
      --data-raw '{
-   "space": "space:cord:47KLJTD6q3AJytGELeCR9RynU7Kuny4wc8ACEUmN4tdW3PN3",
    "schema": {
        "$schema": "http://json-schema.org/draft-07/schema#",
        "title": "test",
@@ -18,12 +14,22 @@ curl --request POST http://localhost:5001/api/v1/schemas \
        }
     }
 }'
+
 {"result":"SUCCESS","schema":"schema:cord:56crMNQCQ3jS5pFahocZ9qLBuj5xEwD5saXpsgNhtZMBatSf"}
 
-curl --request POST "http://localhost:5001/api/v1/space:cord:47KLJTD6q3AJytGELeCR9RynU7Kuny4wc8ACEUmN4tdW3PN3/records" \
+curl --request POST http://localhost:5001/api/v1/spaces --header 'Content-Type: application/json' \
+     --data-raw '{ "schema": "schema:cord:56crMNQCQ3jS5pFahocZ9qLBuj5xEwD5saXpsgNhtZMBatSf",
+      "space": { "title": "test", "description": "ABCD" }
+}'
+
+Output :
+{"result":"SUCCESS","space":"space:cord:4B9Ahyjif9L8UGtoUcGdaS6TbCeMhq9XHAZ6rkmQdPmMtV8K"}
+
+
+curl --request POST "http://localhost:5001/api/v1/space:cord:4B9Ahyjif9L8UGtoUcGdaS6TbCeMhq9XHAZ6rkmQdPmMtV8K/records" \
      --header 'Content-Type: application/json' \
      --data-raw '{
-   "space": "space:cord:47KLJTD6q3AJytGELeCR9RynU7Kuny4wc8ACEUmN4tdW3PN3",
+   "space": "space:cord:4B9Ahyjif9L8UGtoUcGdaS6TbCeMhq9XHAZ6rkmQdPmMtV8K",
    "schema": "schema:cord:56crMNQCQ3jS5pFahocZ9qLBuj5xEwD5saXpsgNhtZMBatSf",
    "title": "Record 1",
    "content": {
@@ -32,3 +38,6 @@ curl --request POST "http://localhost:5001/api/v1/space:cord:47KLJTD6q3AJytGELeC
      "gender": "Female"
    }
 }'
+
+Output:
+{"result":"SUCCESS","stream":{"identifier":"6Ad3dC1HCTVVvZ5ZRSY5VCmy2v9HG3YqxszVedBcRZNixLaT","streamHash":"0xc5d778546952d0d493e29a536669698fd76f9717d0ad6c6935dab5feb06ae967","issuer":"3x4DHc1rxVAEqKWSx1DAAA8wZxLB4VhiRbMV997niBckUwSi","holder":"3x4DHc1rxVAEqKWSx1DAAA8wZxLB4VhiRbMV997niBckUwSi","schema":"56crMNQCQ3jS5pFahocZ9qLBuj5xEwD5saXpsgNhtZMBatSf","link":null,"space":"4B9Ahyjif9L8UGtoUcGdaS6TbCeMhq9XHAZ6rkmQdPmMtV8K","signatureProof":{"keyId":"3x4DHc1rxVAEqKWSx1DAAA8wZxLB4VhiRbMV997niBckUwSi","signature":"0x01a0b73259855067edddd733bcae523665d71fdb57dbf51a30eb0de984b2cad508a51658b28c98f8d2b57393eb65a1ffb973abbafd075c0bc622c87914d7cc7d8a"}}}
